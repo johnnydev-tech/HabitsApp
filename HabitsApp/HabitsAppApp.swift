142d4ae5a3d089b128e2b9050fe21fb195597da1
johnnydev-tech <johnnyfreirejh@hotmail.com>
2025-12-10 12:31:56 -0300

    Initial Commit

---
+++ HabitsApp/HabitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsAppApp.swift
+//  HabitsApp
+//
+//  Created by Johnny Freire on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
